30b6db92790f2aa37ae63eda876d2ab81cf2d265
Rochelle Lee <rochelle.y.lee@gmail.com>
2016-01-18 14:54:24 -0800

    creating basic content for demo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # git-practice
+1. Fork the repo
+2. Clone your fork of this repo
+3. Add this repo as an upstream remote (git remote add upstream <repo URL>)
---
+++ app.js
@@ -0,0 +1,11 @@
+var person = {
+  name: 'Rochelle',
+  email: 'rochelle.lee@hackreactor.com',
+  phone: 5109983957
+};
+
+var sayHello = function(person) {
+  console.log('Hello ' + person.name + '!');
+};
+
+//Write a below function that console logs the email property of person
